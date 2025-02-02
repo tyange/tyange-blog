b17544422a9070f70b0c38c955c084bec78e236d
tyange <usun16@gmail.com>
2025-02-02 13:06:42 +0900

    featured posts

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <main class="mx-auto max-w-4xl px-4 sm:px-12 lg:px-14 mt-4">
+    <main class="mx-auto max-w-4xl px-4 sm:px-12 lg:px-14 mt-6">
       <NuxtPage />
     </main>
   </NuxtLayout>
