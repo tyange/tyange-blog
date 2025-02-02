c101afa1d2d2a6987ba18de610dccfae0502cc51
tyange <usun16@gmail.com>
2025-02-03 08:09:20 +0900

    무한 스크롤 기능 코드를 임시로 테스트

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex flex-col gap-5">
+    <button @click="increase">more item</button>
     <Post v-for="item in postList" :key="item.id" :item="item" />
   </div>
 </template>
@@ -9,10 +10,20 @@ import { SELECTION } from "~/constants/selection";
 
 import Post from "~/components/post-card.vue";
 
-const { data: postList } = await useAsyncData("post-list", () => {
-  return queryCollection("content")
-    .select(...SELECTION)
-    .limit(10)
-    .all();
-});
+const limitter = ref(10);
+
+const increase = () => {
+  limitter.value += limitter.value;
+};
+
+const { data: postList } = await useAsyncData(
+  "post-list",
+  () => {
+    return queryCollection("content")
+      .select(...SELECTION)
+      .limit(limitter.value)
+      .all();
+  },
+  { watch: [limitter] },
+);
 </script>
