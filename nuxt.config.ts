15352db65e187f80b7d641d0ecb43131dc76a2a0
tyange <usun16@gmail.com>
2025-02-03 23:27:40 +0900

    무한 스크롤 기능 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "@vueuse/nuxt"],
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="flex flex-col gap-5">
-    <button @click="increase">more item</button>
+  <div class="mb-10 flex flex-col gap-5">
     <Post v-for="item in postList" :key="item.id" :item="item" />
+    <div
+      v-if="hasMorePost"
+      ref="loader"
+      class="flex items-center justify-center"
+    >
+      <LoaderCircle :size="50" class="animate-spin" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { SELECTION } from "~/constants/selection";
+import { useIntersectionObserver } from "@vueuse/core";
 
 import Post from "~/components/post-card.vue";
 
-const limitter = ref(10);
+import { LoaderCircle } from "lucide-vue-next";
 
-const increase = () => {
+const limitter = ref(5);
+const loader = ref<HTMLElement | null>(null);
+const hasMorePost = ref(true);
+
+const handleIncreaseLimitter = () => {
   limitter.value += limitter.value;
 };
+
+const { stop } = useIntersectionObserver(loader, ([entry]) => {
+  if (entry?.isIntersecting) {
+    handleIncreaseLimitter();
+  }
+});
+
+const { data: postCount } = await useAsyncData("post-count", () => {
+  return queryCollection("content").all();
+});
 
 const { data: postList } = await useAsyncData(
   "post-list",
@@ -25,5 +46,20 @@ const { data: postList } = await useAsyncData(
       .all();
   },
   { watch: [limitter] },
+);
+
+watch(
+  postList,
+  (newValue) => {
+    if (!newValue || !postCount.value) {
+      return;
+    }
+
+    if (newValue.length === postCount.value.length) {
+      stop();
+      hasMorePost.value = false;
+    }
+  },
+  { immediate: true },
 );
 </script>
