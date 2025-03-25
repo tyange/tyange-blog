5677717e96b86a29c3a33a5ee13de3e03d23ddb2
유태양 <taeyang.yu@autowini.com>
2025-03-26 07:57:17 +0900

    pnpm update 후 코드 수정

--- pages/index.vue
+++ pages/index.vue
@@ -29,13 +29,9 @@ import { SELECTION } from "~/constants/selection";
 import FeaturedPost from "~/components/featured-post.vue";
 import PostCard from "~/components/post-card.vue";
 
-// TODO: 현재 @nuxt/content 모듈이 내부적으로 사용 중인 SQLite 관련 코드에서 오류가 있음.
-// 아래 where 메서드에 boolean 값을 인자로 넘길 수 있으나, parsing 과정에서 SQLite에서 인식 가능한 0, 1로 변환되지 않음.
-// 임시 방편으로 0, 1을 전달해서 코드를 작동시키고 추후 v3.1.0(현재 3.0.1)에서 버그가 고쳐지면 그때 boolean으로 값을 변경할 것.
-// (관련 이슈: https://github.com/nuxt/content/issues/3047)
 const { data: featuredPostList } = await useAsyncData("featured-posts", () => {
   return queryCollection("content")
-    .where("featured", "=", 1)
+    .where("featured", "=", true)
     .select(...SELECTION)
     .all();
 });
