b2a3a95330970d7345e18749ced2885c58bb3993
유태양 <taeyang.yu@autowini.com>
2025-03-29 09:32:31 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	{@render children()}
+</div>
+
+<style></style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+	<p>tyange-blog</p>
+	<p>TYANGE-BLOG</p>
+	<p>블로그입니다.</p>
+	<span class="loading loading-sm"></span>
+</div>
+
+<style>
+</style>
