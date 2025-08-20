097f51bff23eaccde085819fc7daba028e42dc8c
tyange <usun16@gmail.com>
2025-08-20 22:43:48 +0900

    fix: change path in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 export default {
   apps: [{
     name: 'tyange-blog',
-    script: '/home/tyange/dev/deploy/tyange-blog/.output/server/index.mjs',
+    script: '.output/server/index.mjs',
     cwd: '/home/tyange/dev/deploy/tyange-blog',
     instances: 1,
     exec_mode: 'fork',
