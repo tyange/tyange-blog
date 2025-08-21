b7751dc0f1075f863428ac2952dc4ff8f2be24f0
tyange <usun16@gmail.com>
2025-08-21 13:32:01 +0900

    change: fonts

---
+++ components/prose/ProseCode.vue
@@ -0,0 +1,14 @@
+<template>
+  <code
+    class="prose-code"
+    :class="$attrs.class"
+  >
+    <slot />
+  </code>
+</template>
+
+<style scoped>
+.prose-code {
+  font-family: 'Monaspace Krypton', monospace;
+}
+</style>
