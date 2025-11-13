86be73d5103c87cbe0ba523f7224c6227a4ee89d
tyange <usun16@gmail.com>
2025-11-14 08:17:21 +0900

    robots.txt 수정 (들여쓰기 주의)

--- server/routes/robots.txt.ts
+++ server/routes/robots.txt.ts
@@ -2,25 +2,25 @@ export default defineEventHandler((event) => {
   setHeader(event, 'Content-Type', 'text/plain; charset=utf-8')
 
   return `User-Agent: *
-        Allow: /
+Allow: /
 
-        Sitemap: https://blog.tyange.xyz/sitemap.xml
+Sitemap: https://blog.tyange.xyz/sitemap.xml
 
-        User-agent: CCBot
-        Disallow: /
+User-agent: CCBot
+Disallow: /
 
-        User-agent: GPTBot
-        Disallow: /
+User-agent: GPTBot
+Disallow: /
 
-        User-agent: Google-Extended
-        Disallow: /
+User-agent: Google-Extended
+Disallow: /
 
-        User-agent: Googlebot
-        Allow: /
+User-agent: Googlebot
+Allow: /
 
-        User-agent: Bingbot
-        Allow: /
+User-agent: Bingbot
+Allow: /
 
-        User-agent: ClaudeBot
-        Allow: /`
+User-agent: ClaudeBot
+Allow: /`
 })
